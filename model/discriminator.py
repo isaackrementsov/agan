9a105844948d1b9111362236819a7cacafb85dd1
Isaac Krementsov <isaackrementsov@gmail.com>
2021-01-08 20:06:52 -0500

    Bug fix

--- model/discriminator.py
+++ model/discriminator.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import keras.backend as K
 
 class Clip(keras.constraints.Constraint):
 
